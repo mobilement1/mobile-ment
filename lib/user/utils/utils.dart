197d2b8fc31a115c987260211c8f73e75e833b0c
ZEENATH-964 <munavarbr@gmail.com>
2025-05-21 17:19:26 +0530

    ui

---
+++ lib/user/utils/utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorStyle {
+  static const black=Colors.black;
+  static const blue=Colors.blue;
+  static const white=Colors.white;
+  static final containerColor= const Color.fromARGB(51, 18, 47, 55);
+
+
+}
