74b573eb2aa074c984b6387e01035edaab1ed346
ZEENATH-964 <munavarbr@gmail.com>
2025-05-26 15:24:13 +0530

    service

--- lib/user/View/UserService/user_service.dart
+++ lib/user/View/UserService/user_service.dart
@@ -35,7 +35,8 @@ class UserService extends StatelessWidget {
                 prefixIcon: icon(Icons.search, Colors.white)
               ),
               
-            )
+            ),
+            Column(children: [],)
           ],
         ),
       )),
